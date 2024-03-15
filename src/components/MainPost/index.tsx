d5e5d83644d5499890d97bb7f3435c547e5b61fb
Vespucci95 <donglonk@naver.com>
2024-03-15 15:49:25 +0900

    🎨 hashTag 바인딩 수정

--- src/components/MainPost/index.tsx
+++ src/components/MainPost/index.tsx
@@ -9,25 +9,29 @@ type Props = {
 const MainPosts = ({ edges }: Props) => {
     return (
         <S.Container>
-            {edges.map(edge => (
-                <S.Wrapper key={edge.node.id}>
-                    <S.Thumbnail
-                        alt={edge.node.id}
-                        image={edge.node.frontmatter.thumbnail.childImageSharp.gatsbyImageData}
-                        draggable={false}
-                    />
-                    <S.PostContent>
-                        <S.Title>{edge.node.frontmatter.title}</S.Title>
-                        <S.Description>{edge.node.frontmatter.description}</S.Description>
-                        <S.Row>
-                            <S.Info>
-                                {edge.node.frontmatter.categories} ∣ {edge.node.frontmatter.tags.join(', ')}
-                            </S.Info>
-                            <S.Date>{edge.node.frontmatter.date}</S.Date>
-                        </S.Row>
-                    </S.PostContent>
-                </S.Wrapper>
-            ))}
+            {edges.map(edge => {
+                const hasHashTag = edge.node.fields.hashTag && edge.node.fields.hashTag.length > 0;
+                return (
+                    <S.Wrapper key={edge.node.id}>
+                        <S.Thumbnail
+                            alt={edge.node.id}
+                            image={edge.node.frontmatter.thumbnail.childImageSharp.gatsbyImageData}
+                            draggable={false}
+                        />
+                        <S.PostContent>
+                            <S.Title>{edge.node.frontmatter.title}</S.Title>
+                            <S.Description>{edge.node.frontmatter.description}</S.Description>
+                            <S.Row>
+                                <S.Info>
+                                    {edge.node.frontmatter.categories}
+                                    {hasHashTag ? ' ∣ ' + edge.node.fields.hashTag.join(',') : null}
+                                </S.Info>
+                                <S.Date>{edge.node.frontmatter.date}</S.Date>
+                            </S.Row>
+                        </S.PostContent>
+                    </S.Wrapper>
+                );
+            })}
         </S.Container>
     );
 };
