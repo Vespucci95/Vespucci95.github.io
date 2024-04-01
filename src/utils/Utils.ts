fce07ba3ed6b0edab79a6ac5b7ca9f78f4a49453
Vespucci95 <donglonk@naver.com>
2024-04-01 18:24:29 +0900

    🎨 hashTag regex 수정

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -1,6 +1,6 @@
 export const Utils = {
     convertHashTag(html: string) {
-        return html.replace(/\s#[A-Za-z0-9가-힣]+/gm, val => {
+        return html.replace(/(?:^|\s)#([\w가-힣\\-]+)/gm, val => {
             const tagName = encodeURIComponent(`${val.trim().replace(/#/, '')}`);
             return `<a href="/hashtag/${tagName}">${val}</a>`;
         });
